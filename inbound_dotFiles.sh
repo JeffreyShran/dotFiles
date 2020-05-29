0343927ee3823b0788fb070808ac65ce7425a24b
JeffreyShran <10995361+JeffreyShran@users.noreply.github.com>
2020-05-29 11:33:45 +0100

    Update inbound_dotFiles.sh

--- inbound_dotFiles.sh
+++ inbound_dotFiles.sh
@@ -11,14 +11,14 @@ if ! dpkg -s curl >/dev/null 2>&1; then
   apt-get install -qy curl
 fi
 
-if ! curl --create-dirs "https://raw.githubusercontent.com/JeffreyShran/dotFiles/master/.bash_aliases" -o "~/.bash_aliases"; then
+if ! curl --create-dirs "https://raw.githubusercontent.com/JeffreyShran/dotFiles/master/.bash_aliases" -o "$HOME/.bash_aliases"; then
   printf '(bash_aliases) Curl failed with error code "%d"\n' "$?" >&2
 fi
 
-if ! curl --create-dirs "https://raw.githubusercontent.com/JeffreyShran/dotFiles/master/.bash_profile" -o "~/.bash_profile"; then
+if ! curl --create-dirs "https://raw.githubusercontent.com/JeffreyShran/dotFiles/master/.bash_profile" -o "$HOME/.bash_profile"; then
   printf '(bash_profile) Curl failed with error code "%d"\n' "$?" >&2
 fi
 
-if ! curl --create-dirs "https://raw.githubusercontent.com/JeffreyShran/dotFiles/master/.bashrc" -o "~/.bashrc"; then
+if ! curl --create-dirs "https://raw.githubusercontent.com/JeffreyShran/dotFiles/master/.bashrc" -o "$HOME/.bashrc"; then
   printf '(bashrc) Curl failed with error code "%d"\n' "$?" >&2
 fi
